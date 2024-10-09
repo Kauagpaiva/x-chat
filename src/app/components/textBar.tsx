75285c293d204b3e544bc91679a0b344090bc6a9
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-10-09 19:39:04 -0300

    ajuste altura textbar

--- src/app/components/textBar.tsx
+++ src/app/components/textBar.tsx
@@ -20,7 +20,7 @@ export default function TextBar({ onSendMessage }: TextBarProps) {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Escreva a sua mensagem aqui"
-        className="w-full h-8 rounded-[10px] px-4 py-5 bg-zinc-600 hover:bg-zinc-500 placeholder-gray-100"
+        className="w-full rounded-[10px] px-4 py-3 bg-zinc-600 hover:bg-zinc-500 placeholder-gray-100"
       />
       <button
         className="px-5 py-2 rounded-[50px] bg-[#3ea59f] hover:bg-[#32847f] text-fuchsia-50"
